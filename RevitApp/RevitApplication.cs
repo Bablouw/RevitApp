c3948a744b39490fd493a0eed84087fbf7c618f6
MIX PC <vort12@yandex.ru>
2025-04-16 17:07:09 +0300

    Добавьте файлы проекта.

---
+++ RevitApp/RevitApplication.cs
@@ -0,0 +1,42 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitApp
+{
+
+    internal class RevitApplication : IExternalApplication
+    {
+        static AddInId addinId = new AddInId(new Guid("2DE669CB-D848-478F-BA3F-0850C46033E4"));
+        private readonly string assemblyPath = Assembly.GetExecutingAssembly().Location;
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            string tabName = "My custom tab";
+            application.CreateRibbonTab(tabName);
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel(tabName, "Автоматизация");
+            AddButton(ribbonPanel, "Button 1", assemblyPath, "RevitApp.TestPlagin", "Подсказка");
+            return Result.Succeeded;
+        }
+
+        private void AddButton(RibbonPanel ribbonPanel, string buttonName, string path, string linkToCommand, string toolTip)
+        {
+            PushButtonData buttonData = new PushButtonData(
+                buttonName,
+                buttonName,
+                path,
+                linkToCommand);
+            PushButton Button = ribbonPanel.AddItem(buttonData) as PushButton;
+            Button.ToolTip = toolTip;
+        }
+    }
+}
---
+++ RevitApp/TestPlagin.cs
@@ -0,0 +1,36 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Configuration.Assemblies;
+using System.Linq;
+using System.Reflection;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+
+
+namespace RevitApp
+{
+    [Autodesk.Revit.Attributes.TransactionAttribute(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class TestPlagin : IExternalCommand
+    {
+        static AddInId addinId = new AddInId(new Guid("2DE669CB-D848-478F-BA3F-0850C46033E4"));
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            Reference myRef = uidoc.Selection.PickObject(ObjectType.Element, "Выберите элемент для вывода его Id");
+            Element element = doc.GetElement(myRef);
+            ElementId id = element.Id;
+            TaskDialog.Show("Hello world!", $"id элемента: {id.ToString()}");
+            return Result.Succeeded;
+
+        }
+    }
+}
