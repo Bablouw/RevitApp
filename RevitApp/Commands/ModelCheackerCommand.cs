2b46af9d8625df5e9b3b74c33599942bf4f769db
Bablouw <vort12@yandex.ru>
2025-07-02 10:59:35 +0300

    надо проверить вывод списка видов

--- RevitApp/Commands/ModelCheackerCommand.cs
+++ RevitApp/Commands/ModelCheackerCommand.cs
@@ -2,6 +2,7 @@
 using Autodesk.Revit.DB.Events;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
+using RevitApp.ModelChecker.Model;
 using RevitApp.SumParamPlugin.View;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,7 @@ using System.Reflection;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
@@ -22,21 +24,18 @@ namespace RevitApp
     public class ModelCheackerCommand : IExternalCommand
     {
         static AddInId addinId = new AddInId(new Guid("2DE669CB-D848-478F-BA3F-0850C46033E4"));
-        private SumParamView _view;
+
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             if (RevitApi.UIApplication == null)
             {
                 RevitApi.Initialize(commandData);
             }
-            var viewModel = new ViewModel();
-
-        }
-        private void OnDocumentClosed(object sender, DocumentClosedEventArgs e)
-        {
-            _view.Close();
-            _view = null;
-            RevitApi.Application.DocumentClosed -= OnDocumentClosed;
+            ModelOpener opener = new ModelOpener();
+            Document doc = opener.OpenModel("D:\\239_05_WS_AYAKS_WD_R21.rvt", commandData.Application.Application, out string errormessage);
+            ViewLister viewLister = new ViewLister();
+            MessageBox.Show(string.Join("\n" , viewLister.GetViewNames(doc)));
+            return Result.Succeeded;
         }
     }
 }
---
+++ RevitApp/ModelChecker/Model/ViewLister.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+
+namespace RevitApp.ModelChecker.Model
+{
+    public class ViewLister
+    {
+        private Document PreparedDocument;
+
+        public List<string> GetViewNames(Document doc)
+        {
+            if(doc == null)
+            {
+                return new List<string>();
+            }
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(View));
+            List<View> views = collector.Cast<View>().ToList();
+            views = views.Where(v => !v.IsTemplate).ToList();
+            List<string> viewNames = views.Select(v => v.Name).ToList();
+            return viewNames;
+        }
+    }
+}
